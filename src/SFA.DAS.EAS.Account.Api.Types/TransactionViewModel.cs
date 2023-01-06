61bfb1af0db7010ca3384729bf98830b73bd5c14
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-06 10:26:36 +0000

    Removed unused classes and props

--- src/SFA.DAS.EAS.Account.Api.Types/TransactionViewModel.cs
+++ src/SFA.DAS.EAS.Account.Api.Types/TransactionViewModel.cs
@@ -6,12 +6,11 @@ namespace SFA.DAS.EAS.Account.Api.Types
     public class TransactionViewModel
     {
         public string Description { get; set; }
-        public TransactionItemType TransactionType { get; set; }
+        
         public DateTime TransactionDate { get; set; }
         public DateTime DateCreated { get; set; }
         public decimal Amount { get; set; }
         public decimal Balance { get; set; }
-        public List<TransactionViewModel> SubTransactions { get; set; }
         public string ResourceUri { get; set; }
     }
 }
