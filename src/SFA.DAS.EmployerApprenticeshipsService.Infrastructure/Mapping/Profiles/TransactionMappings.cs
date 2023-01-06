f0a9fa3b0a6fd798d093cee698d6b851cf70c540
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-06 08:37:55 +0000

    Removing unused domain models for Payments

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Mapping/Profiles/TransactionMappings.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Mapping/Profiles/TransactionMappings.cs
@@ -1,7 +1,5 @@
 ﻿using AutoMapper;
 using SFA.DAS.EAS.Domain.Models.ExpiredFunds;
-using SFA.DAS.EAS.Domain.Models.Levy;
-using SFA.DAS.EAS.Domain.Models.Payments;
 using SFA.DAS.EAS.Domain.Models.Transaction;
 using SFA.DAS.EAS.Domain.Models.Transfers;
 
@@ -12,7 +10,6 @@ namespace SFA.DAS.EAS.Infrastructure.Mapping.Profiles
         public TransactionMappings()
         {
             CreateMap<TransactionEntity, TransactionLine>();
-            CreateMap<TransactionEntity, PaymentTransactionLine>();
             CreateMap<TransactionEntity, TransferTransactionLine>();
             CreateMap<TransactionEntity, ExpiredFundTransactionLine>();
         }
