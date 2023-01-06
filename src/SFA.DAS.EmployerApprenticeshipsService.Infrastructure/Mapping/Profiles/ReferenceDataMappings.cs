8aa9e2a6335c6f00ae56f3eefacada1245ae50f0
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-06 08:34:08 +0000

    Removing unused domain models for Organisation

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Mapping/Profiles/ReferenceDataMappings.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Mapping/Profiles/ReferenceDataMappings.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using SFA.DAS.ReferenceData.Api.Client.Dto;
 using SFA.DAS.ReferenceData.Types.DTO;
 
 namespace SFA.DAS.EAS.Infrastructure.Mapping.Profiles
@@ -10,8 +9,6 @@ namespace SFA.DAS.EAS.Infrastructure.Mapping.Profiles
         {
             CreateMap<Charity, Domain.Models.ReferenceData.Charity>();
             CreateMap<PublicSectorOrganisation, Domain.Models.ReferenceData.PublicSectorOrganisation>();
-            CreateMap<Organisation, Domain.Models.ReferenceData.OrganisationName>();
-            CreateMap<Address, Domain.Models.Organisation.Address>();
         }
     }
 }
