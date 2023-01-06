f213dc5ddfdfc808c583bc13566f241ef4a7764c
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-06 08:25:42 +0000

    Removing unused domain models for EmployerAgreement

--- src/SFA.DAS.EAS.Api/Mappings/AccountMappings.cs
+++ src/SFA.DAS.EAS.Api/Mappings/AccountMappings.cs
@@ -16,8 +16,6 @@ namespace SFA.DAS.EAS.Account.Api.Mappings
                 .ForMember(target => target.DasAccountName, opt => opt.MapFrom(src => src.Name));
             CreateMap<LevyDeclarationView, LevyDeclarationViewModel>()
                 .ForMember(target => target.PayeSchemeReference, opt => opt.MapFrom(src => src.EmpRef));
-
-            CreateMap<Domain.Models.EmployerAgreement.EmployerAgreementView, EmployerAgreementView>();
         }
     }
 }
