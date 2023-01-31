f812a72e01d6a694a52d2b08fe71d92488e021c6
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-01-31 16:49:48 +0000

    Added DateTimeRegistration

---
+++ src/SFA.DAS.EmployerFinance/ServiceRegistration/DateTimeServiceRegistrations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.EmployerFinance.Interfaces;
+using SFA.DAS.EmployerFinance.Time;
+
+namespace SFA.DAS.EmployerFinance.ServiceRegistration
+{
+    public static class DateTimeServiceRegistrations
+    {
+        public static IServiceCollection AddDateTimeServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var cloudCurrentTime = configuration.GetValue<string>("CurrentTime");
+
+            if(!DateTime.TryParse(cloudCurrentTime, out var currentTime))
+            {
+                currentTime= DateTime.Now;
+            }
+
+            services.AddSingleton<ICurrentDateTime>(_ => new CurrentDateTime(currentTime));
+
+            return services;
+        }
+
+    }
+}
--- src/SFA.DAS.EmployerFinance/Time/CurrentDateTime.cs
+++ src/SFA.DAS.EmployerFinance/Time/CurrentDateTime.cs
@@ -1,32 +1,20 @@
 ﻿using System;
-using System.Configuration;
 using SFA.DAS.EmployerFinance.Interfaces;
 
 namespace SFA.DAS.EmployerFinance.Time
 {
     public sealed class CurrentDateTime : ICurrentDateTime
     {
-        public DateTime Now => _now ?? DateTime.Now;
-
-        private readonly DateTime? _now;
+        public DateTime Now { get; }
 
         public CurrentDateTime()
         {
-            var setting = ConfigurationManager.AppSettings["CurrentTime"];
-
-            if (DateTime.TryParse(setting, out var now))
-            {
-                _now = now;
-            }
+            Now = DateTime.UtcNow;
         }
 
-        /// <summary>
-        /// For testing use only so a reference date can be supplied instead of using DateTime.UtcNow.
-        /// </summary>
-        /// <param name="referenceDate">Reference date to be used for testing only.</param>
-        public CurrentDateTime(DateTime referenceDate)
+        public CurrentDateTime(DateTime time)
         {
-            _now = referenceDate;
+            Now = time;
         }
     }
 }
