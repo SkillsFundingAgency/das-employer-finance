3ed89e1f2e3cff0cd30e5648fe0ef5b33c4376af
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-01 10:22:46 +0000

    Commenting out and will delete later

--- src/SFA.DAS.EmployerFinance.Web/Global.asax.cs
+++ src/SFA.DAS.EmployerFinance.Web/Global.asax.cs
@@ -1,119 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Net;
-using System.Reflection;
-using System.Security.Claims;
-using Microsoft.ApplicationInsights;
-using Microsoft.ApplicationInsights.Extensibility;
-using NLog;
-using SFA.DAS.Audit.Client;
-using SFA.DAS.Audit.Client.Web;
-using SFA.DAS.Audit.Types;
-using SFA.DAS.AutoConfiguration;
-using SFA.DAS.EmployerFinance.Startup;
-using SFA.DAS.EmployerFinance.Web.Logging;
-using SFA.DAS.EmployerFinance.Web.ViewModels;
-using SFA.DAS.EmployerUsers.WebClientComponents;
-using SFA.DAS.Logging;
-using SFA.DAS.Web.Policy;
+﻿//MAP-192 - Commenting out and will delete later
 
-namespace SFA.DAS.EmployerFinance.Web
-{
-    public class MvcApplication : HttpApplication
-    {
-        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+//using System;
+//using System.Collections.Generic;
+//using System.Configuration;
+//using System.Net;
+//using System.Reflection;
+//using System.Security.Claims;
+//using Microsoft.ApplicationInsights;
+//using Microsoft.ApplicationInsights.Extensibility;
+//using NLog;
+//using SFA.DAS.Audit.Client;
+//using SFA.DAS.Audit.Client.Web;
+//using SFA.DAS.Audit.Types;
+//using SFA.DAS.AutoConfiguration;
+//using SFA.DAS.EmployerFinance.Startup;
+//using SFA.DAS.EmployerFinance.Web.Logging;
+//using SFA.DAS.EmployerFinance.Web.ViewModels;
+//using SFA.DAS.EmployerUsers.WebClientComponents;
+//using SFA.DAS.Logging;
+//using SFA.DAS.Web.Policy;
 
-        protected void Application_Start()
-        {
-            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
-            AntiForgeryConfig.RequireSsl = true;
-            AreaRegistration.RegisterAllAreas();
-            BinderConfig.RegisterBinders(ModelBinders.Binders);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            LoggingConfig.ConfigureLogging();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["InstrumentationKey"];
-            WebMessageBuilders.Register();
-            WebMessageBuilders.UserIdClaim = DasClaimTypes.Id;
-            WebMessageBuilders.UserEmailClaim = DasClaimTypes.Email;
+//namespace SFA.DAS.EmployerFinance.Web
+//{
+//    public class MvcApplication : HttpApplication
+//    {
+//        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
-            AuditMessageFactory.RegisterBuilder(m =>
-            {
-                m.Source = new Source
-                {
-                    Component = "EmployerFinance-Web",
-                    System = "EmployerFinance",
-                    Version = typeof(MvcApplication).Assembly.GetName().Version.ToString()
-                };
-            });
+//        protected void Application_Start()
+//        {
+//            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+//            AntiForgeryConfig.RequireSsl = true;
+//            AreaRegistration.RegisterAllAreas();
+//            BinderConfig.RegisterBinders(ModelBinders.Binders);
+//            BundleConfig.RegisterBundles(BundleTable.Bundles);
+//            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+//            LoggingConfig.ConfigureLogging();
+//            RouteConfig.RegisterRoutes(RouteTable.Routes);
+//            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["InstrumentationKey"];
+//            WebMessageBuilders.Register();
+//            WebMessageBuilders.UserIdClaim = DasClaimTypes.Id;
+//            WebMessageBuilders.UserEmailClaim = DasClaimTypes.Email;
 
-            var container = StructuremapMvc.StructureMapDependencyScope.Container;
+//            AuditMessageFactory.RegisterBuilder(m =>
+//            {
+//                m.Source = new Source
+//                {
+//                    Component = "EmployerFinance-Web",
+//                    System = "EmployerFinance",
+//                    Version = typeof(MvcApplication).Assembly.GetName().Version.ToString()
+//                };
+//            });
 
-            var environmentService = container.GetInstance<IEnvironmentService>();
+//            var container = StructuremapMvc.StructureMapDependencyScope.Container;
 
-            if (environmentService.IsCurrent(DasEnv.LOCAL))
-            {
-                SystemDetailsViewModel.EnvironmentName = DasEnv.LOCAL.ToString();
-                SystemDetailsViewModel.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            }
-            if (environmentService.IsCurrent(DasEnv.AT))
-            {
-                SystemDetailsViewModel.EnvironmentName = DasEnv.AT.ToString();
-                SystemDetailsViewModel.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            }
-            if (environmentService.IsCurrent(DasEnv.TEST))
-            {
-                SystemDetailsViewModel.EnvironmentName = DasEnv.TEST.ToString();
-                SystemDetailsViewModel.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            }
+//            var environmentService = container.GetInstance<IEnvironmentService>();
 
-            DependencyResolver.Current.GetService<IStartup>().StartAsync().GetAwaiter().GetResult();
-        }
+//            if (environmentService.IsCurrent(DasEnv.LOCAL))
+//            {
+//                SystemDetailsViewModel.EnvironmentName = DasEnv.LOCAL.ToString();
+//                SystemDetailsViewModel.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+//            }
+//            if (environmentService.IsCurrent(DasEnv.AT))
+//            {
+//                SystemDetailsViewModel.EnvironmentName = DasEnv.AT.ToString();
+//                SystemDetailsViewModel.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+//            }
+//            if (environmentService.IsCurrent(DasEnv.TEST))
+//            {
+//                SystemDetailsViewModel.EnvironmentName = DasEnv.TEST.ToString();
+//                SystemDetailsViewModel.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+//            }
 
-        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
-        {
-            new HttpContextPolicyProvider(new List<IHttpContextPolicy> { new ResponseHeaderRestrictionPolicy() })
-                .Apply(new HttpContextWrapper(HttpContextHelper.Current), PolicyConcern.HttpResponse);
-        }
+//            DependencyResolver.Current.GetService<IStartup>().StartAsync().GetAwaiter().GetResult();
+//        }
 
-        protected void Application_End()
-        {
-            DependencyResolver.Current.GetService<IStartup>().StopAsync().GetAwaiter().GetResult();
-        }
+//        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+//        {
+//            new HttpContextPolicyProvider(new List<IHttpContextPolicy> { new ResponseHeaderRestrictionPolicy() })
+//                .Apply(new HttpContextWrapper(HttpContextHelper.Current), PolicyConcern.HttpResponse);
+//        }
 
-        protected void Application_Error(object sender, EventArgs e)
-        {
-            var exception = Server.GetLastError();
+//        protected void Application_End()
+//        {
+//            DependencyResolver.Current.GetService<IStartup>().StopAsync().GetAwaiter().GetResult();
+//        }
 
-            if (exception is HttpException httpException && httpException.GetHttpCode() == (int)HttpStatusCode.NotFound)
-            {
-                return;
-            }
+//        protected void Application_Error(object sender, EventArgs e)
+//        {
+//            var exception = Server.GetLastError();
 
-            Dictionary<string, object> properties = null;
+//            if (exception is HttpException httpException && httpException.GetHttpCode() == (int)HttpStatusCode.NotFound)
+//            {
+//                return;
+//            }
 
-            try
-            {
-                properties = new Dictionary<string, object>
-                {
-                    ["HttpMethod"] = Request.HttpMethod,
-                    ["IsAuthenticated"] = Request.IsAuthenticated,
-                    ["Url"] = Request.Url.PathAndQuery,
-                    ["UrlReferrer"] = Request.UrlReferrer?.PathAndQuery
-                };
-            }
-            catch (Exception)
-            {
-                // Request not available
-            }
+//            Dictionary<string, object> properties = null;
 
-            var message = exception.GetMessage();
-            var telemetryClient = new TelemetryClient();
+//            try
+//            {
+//                properties = new Dictionary<string, object>
+//                {
+//                    ["HttpMethod"] = Request.HttpMethod,
+//                    ["IsAuthenticated"] = Request.IsAuthenticated,
+//                    ["Url"] = Request.Url.PathAndQuery,
+//                    ["UrlReferrer"] = Request.UrlReferrer?.PathAndQuery
+//                };
+//            }
+//            catch (Exception)
+//            {
+//                // Request not available
+//            }
 
-            Logger.Error(exception, message, properties);
-            telemetryClient.TrackException(exception);
-        }
-    }
-}
+//            var message = exception.GetMessage();
+//            var telemetryClient = new TelemetryClient();
+
+//            Logger.Error(exception, message, properties);
+//            telemetryClient.TrackException(exception);
+//        }
+//    }
+//}
