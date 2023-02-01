6fce37fddcdbf76b507c5a1bba462603856813c4
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-01 16:17:02 +0000

    Added IgnoreMapAttribute

--- src/SFA.DAS.EmployerFinance.Web/Attributes/IgnoreMapAttribute.cs
+++ src/SFA.DAS.EmployerFinance.Web/Attributes/IgnoreMapAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace SFA.DAS.EmployerFinance.Web.Attributes
 {
+    // Summary:
+    // Ignore this member for validation and skip during mapping
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class IgnoreMapAttribute : Attribute
     {
     }
