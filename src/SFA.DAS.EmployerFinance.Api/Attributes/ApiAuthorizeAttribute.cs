213720d70ef6642b4ac15dcb368c8a87a3a80d3a
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 14:41:12 +0000

    Adding Authorization Extension

--- src/SFA.DAS.EmployerFinance.Api/Attributes/ApiAuthorizeAttribute.cs
+++ src/SFA.DAS.EmployerFinance.Api/Attributes/ApiAuthorizeAttribute.cs
@@ -7,9 +7,9 @@ namespace SFA.DAS.EmployerFinance.Api.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class ApiAuthorizeAttribute : Microsoft.AspNetCore.Authorization.AuthorizeAttribute
     {
-        protected override bool IsAuthorized(HttpActionContext actionContext)
-        {
-            return actionContext.Request.RequestUri.IsLoopback || base.IsAuthorized(actionContext);
-        }
+        //protected override bool IsAuthorized(HttpActionContext actionContext)
+        //{
+        //    return actionContext.Request.RequestUri.IsLoopback || base.IsAuthorized(actionContext);
+        //}
     }
 }
