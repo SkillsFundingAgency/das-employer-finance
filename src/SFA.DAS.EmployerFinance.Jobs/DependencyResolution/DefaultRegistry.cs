536f376833bad5e0f9b2cd2997ae6f62157d814e
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 11:23:18 +0000

    Adding required libraries in EmployerFinance.Jobs

--- src/SFA.DAS.EmployerFinance.Jobs/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.EmployerFinance.Jobs/DependencyResolution/DefaultRegistry.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Configuration;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using SFA.DAS.EmployerFinance.Configuration;
+using SFA.DAS.EmployerFinance.Data;
 using StructureMap;
 
 namespace SFA.DAS.EmployerFinance.Jobs.DependencyResolution
@@ -14,6 +22,47 @@ namespace SFA.DAS.EmployerFinance.Jobs.DependencyResolution
                 s.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith("SFA.DAS"));
                 s.RegisterConcreteTypesAgainstTheFirstInterface();
             });
+
+            For<ILoggerFactory>().Use(() => new LoggerFactory().AddNLog()).Singleton();
+            For<ILogger>().Use(c => c.GetInstance<ILoggerFactory>().CreateLogger(c.ParentType));
+            For<EmployerFinanceDbContext>().Use(c => GetDbContext(c));
+            //For<IRunOnceJobsService>().Use<RunOnceJobsService>();
+        }
+
+        private static EmployerFinanceDbContext GetDbContext(IContext context)
+        {
+            var environmentName = ConfigurationManager.AppSettings["EnvironmentName"];
+
+            var connectionString = GetConnectionString(context);
+            var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            bool useManagedIdentity = !connectionStringBuilder.IntegratedSecurity && string.IsNullOrEmpty(connectionStringBuilder.UserID);
+
+            var optionsBuilder = new DbContextOptionsBuilder<EmployerFinanceDbContext>();
+
+
+            if (useManagedIdentity)
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var accessToken = azureServiceTokenProvider.GetAccessTokenAsync(AzureResource).Result;
+                var sqlConnection = new SqlConnection
+                {
+                    ConnectionString = connectionString,
+                    AccessToken = accessToken,
+                };
+
+                optionsBuilder.UseSqlServer(sqlConnection);
+            }
+            else
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+
+            return new EmployerFinanceDbContext(optionsBuilder.Options);
+        }
+
+        private static string GetConnectionString(IContext context)
+        {
+            return context.GetInstance<EmployerFinanceConfiguration>().DatabaseConnectionString;
         }
     }
 }
--- src/SFA.DAS.EmployerFinance.Jobs/NServiceBusStartup.cs
+++ src/SFA.DAS.EmployerFinance.Jobs/NServiceBusStartup.cs
@@ -5,13 +5,10 @@ using NServiceBus;
 using SFA.DAS.EmployerFinance.Configuration;
 using SFA.DAS.EmployerFinance.Extensions;
 using SFA.DAS.EmployerFinance.Startup;
-using SFA.DAS.NServiceBus;
-using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
-using SFA.DAS.NServiceBus.Configuration.NLog;
-using SFA.DAS.NServiceBus.SqlServer.Configuration;
-using SFA.DAS.NServiceBus.Configuration.StructureMap;
-using StructureMap;
 using SFA.DAS.NServiceBus.Configuration;
+using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
+using SFA.DAS.NServiceBus.SqlServer.Configuration;
+using StructureMap;
 
 namespace SFA.DAS.EmployerFinance.Jobs
 {
