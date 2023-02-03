91b4be508168abd90895c915a9304309ffbf0c46
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 11:58:01 +0000

    Added EmployerFinanceOuterApi

---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/EmployerFinanceOuterApiRegistry.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.EmployerFinance.Configuration;
+using SFA.DAS.EmployerFinance.Infrastructure;
+using SFA.DAS.EmployerFinance.Interfaces.OuterApi;
+using System.Net.Http;
+using StructureMap;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class EmployerFinanceOuterApiRegistry :Registry
+    {
+        public EmployerFinanceOuterApiRegistry()
+        {
+            For<EmployerFinanceOuterApiConfiguration>().Use(c => c.GetInstance<EmployerFinanceConfiguration>().EmployerFinanceOuterApiConfiguration).Singleton();
+            For<IOuterApiClient>().Use<OuterApiClient>().Ctor<HttpClient>().Is(new HttpClient()).Singleton();
+        }
+    }
+}
