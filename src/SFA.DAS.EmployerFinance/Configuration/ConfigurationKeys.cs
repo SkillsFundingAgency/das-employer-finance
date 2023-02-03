19adf6ededd468fc237fefce2ead86d7d46c996d
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 14:17:58 +0000

    Adding  AzureActiveDirectoryApiConfiguration = "AzureADApiAuthentication"; config key Adding AuthenticationExtension

--- src/SFA.DAS.EmployerFinance/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.EmployerFinance/Configuration/ConfigurationKeys.cs
@@ -5,5 +5,6 @@
         public const string EmployerFinance = "SFA.DAS.EmployerFinance";
         public const string Features = "SFA.DAS.EmployerFinance.Features";
         public const string NotificationsApiClient = "SFA.DAS.EmployerFinance.Notifications";
+        public const string AzureActiveDirectoryApiConfiguration = "AzureADApiAuthentication";
     }
 }
