2f19027adc6c48c3dbc7ed6cabfc34c1bc3e4eaa
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 13:47:08 +0000

    Adding LoggingRegistry

---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/LoggerRegistry.cs
@@ -0,0 +1,13 @@
+﻿using StructureMap;
+using SFA.DAS.NLog.Logger;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class LoggerRegistry :Registry
+    {
+        public LoggerRegistry()
+        {
+            For<ILog>().Use(c => new NLogLogger(c.ParentType, c.GetInstance<ILoggingContext>(), null)).AlwaysUnique();
+        }
+    }
+}
