59e79d8daf758d3f7433f695aca79297bca590d7
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 13:43:52 +0000

    Adding DateTime Registry

---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/DateTimeRegistry.cs
@@ -0,0 +1,13 @@
+﻿using StructureMap;
+using StructureMap.Pipeline;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class DateTimeRegistry :Registry
+    {
+        public DateTimeRegistry()
+        {
+            Policies.Add<CurrentDatePolicy>();
+        }
+    }
+}
