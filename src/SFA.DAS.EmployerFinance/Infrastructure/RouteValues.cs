cfc49a618c27a15a5641f47892324724b3f83173
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 16:57:07 +0000

    Creating RouteValues

---
+++ src/SFA.DAS.EmployerFinance/Infrastructure/RouteValues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerFinance.Infrastructure
+{
+    public class RouteValues
+    {
+        public const string EmployerAccountId = "employerAccountId";
+
+        public const string Ukprn = "ukprn";
+
+        public const string ExternalId = "externalId";
+        public const string EncodedAccountId = "hashedAccountId";
+    }
+}
