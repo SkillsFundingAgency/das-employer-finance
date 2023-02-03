912c6a1d078abf02e034b712e75b90fb78f178c0
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 13:37:52 +0000

    Adding DataRegistry and HashingRegistry

---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/DataRegistry.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Data.SqlClient;
+using NServiceBus.Persistence;
+using SFA.DAS.EmployerFinance.Configuration;
+using SFA.DAS.EmployerFinance.Data;
+using SFA.DAS.EmployerFinance.MarkerInterfaces;
+using SFA.DAS.HashingService;
+using SFA.DAS.NServiceBus.Features.ClientOutbox.Data;
+using SFA.DAS.UnitOfWork.Context;
+using System.Data.Common;
+using System;
+using StructureMap;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class DataRegistry : Registry
+    {
+        private const string AzureResource = "https://database.windows.net/";
+
+        public DataRegistry()
+        {
+            var environmentName = ConfigurationManager.AppSettings["EnvironmentName"];
+
+            For<DbConnection>().Use($"Build DbConnection", c =>
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+                return environmentName.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase)
+                    ? new SqlConnection(GetEmployerFinanceConnectionString(c))
+                    : new SqlConnection
+                    {
+                        ConnectionString = GetEmployerFinanceConnectionString(c),
+                        AccessToken = azureServiceTokenProvider.GetAccessTokenAsync(AzureResource).Result
+                    };
+            });
+
+            For<EmployerFinanceDbContext>().Use(c => GetDbContext(c));
+        }
+
+        private EmployerFinanceDbContext GetDbContext(IContext context)
+        {
+            var unitOfWorkContext = context.GetInstance<IUnitOfWorkContext>();
+
+            var optionsBuilder = new DbContextOptionsBuilder<EmployerFinanceDbContext>();
+            var connectionString = GetEmployerFinanceConnectionString(context);
+            optionsBuilder.UseSqlServer(connectionString);
+            return new EmployerFinanceDbContext(optionsBuilder.Options);
+        }
+
+        private string GetEmployerFinanceConnectionString(IContext context)
+        {
+            return context.GetInstance<EmployerFinanceConfiguration>().DatabaseConnectionString;
+        }
+
+    }
+}
---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/HashingRegistry.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using SFA.DAS.EmployerFinance.Configuration;
+using SFA.DAS.EmployerFinance.MarkerInterfaces;
+using SFA.DAS.HashingService;
+using StructureMap;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class HashingRegistry : Registry
+    {
+        public HashingRegistry()
+        {
+            For<IHashingService>()
+                .Use(c =>
+                    new HashingService.HashingService(
+                        c.GetInstance<EmployerFinanceConfiguration>().AllowedHashstringCharacters,
+                        c.GetInstance<EmployerFinanceConfiguration>().Hashstring));
+
+            For<IPublicHashingService>()
+                .Use<MarkerInterfaceWrapper>()
+                .Ctor<IHashingService>()
+                .Is(c =>
+                    new HashingService.HashingService(
+                        c.GetInstance<EmployerFinanceConfiguration>().PublicAllowedHashstringCharacters,
+                        c.GetInstance<EmployerFinanceConfiguration>().PublicHashstring)
+                );
+
+            For<IAccountLegalEntityPublicHashingService>()
+                .Use<MarkerInterfaceWrapper>()
+                .Ctor<IHashingService>()
+                .Is(c =>
+                    new HashingService.HashingService(
+                        c.GetInstance<EmployerFinanceConfiguration>().PublicAllowedAccountLegalEntityHashstringCharacters,
+                        c.GetInstance<EmployerFinanceConfiguration>().PublicAllowedAccountLegalEntityHashstringSalt));
+
+        }
+    }
+
+    public class MarkerInterfaceWrapper
+        : IAccountLegalEntityPublicHashingService,
+            IPublicHashingService
+    {
+        private IHashingService _hashingServiceWithCorrectValuesForMarkerInterface;
+
+        public MarkerInterfaceWrapper(IHashingService hashingServiceWithCorrectValuesForMarkerInterface)
+        {
+            _hashingServiceWithCorrectValuesForMarkerInterface = hashingServiceWithCorrectValuesForMarkerInterface;
+        }
+
+        public string HashValue(long id)
+        {
+            return _hashingServiceWithCorrectValuesForMarkerInterface.HashValue(id);
+        }
+
+        public string HashValue(Guid id)
+        {
+            return _hashingServiceWithCorrectValuesForMarkerInterface.HashValue(id);
+        }
+
+        public string HashValue(string id)
+        {
+            return _hashingServiceWithCorrectValuesForMarkerInterface.HashValue(id);
+        }
+
+        public long DecodeValue(string id)
+        {
+            return _hashingServiceWithCorrectValuesForMarkerInterface.DecodeValue(id);
+        }
+
+        public Guid DecodeValueToGuid(string id)
+        {
+            return _hashingServiceWithCorrectValuesForMarkerInterface.DecodeValueToGuid(id);
+        }
+
+        public string DecodeValueToString(string id)
+        {
+            return _hashingServiceWithCorrectValuesForMarkerInterface.DecodeValueToString(id);
+        }
+
+        public bool TryDecodeValue(string input, out long output)
+        {
+            return _hashingServiceWithCorrectValuesForMarkerInterface.TryDecodeValue(
+                input,
+                out output);
+        }
+    }
+}
