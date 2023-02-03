a4c86ff7c4b1d56754d888318600988c70fda5a2
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-03 09:31:31 +0000

    Creating HomeOrchestrator and OrchestratorResponse classes

---
+++ src/SFA.DAS.EmployerFinance.Web/Orchestrators/HomeOrchestrator.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace SFA.DAS.EmployerFinance.Web.Orchestrators
+{
+    public class HomeOrchestrator
+    {
+        private readonly IMediator _mediator;
+
+        public HomeOrchestrator() { }   
+
+        public HomeOrchestrator(IMediator mediator)
+        {
+            _mediator = mediator;   
+        }
+    }
+}
