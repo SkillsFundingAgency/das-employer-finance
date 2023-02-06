b2303581bc4158d85683d458fad60de7ef0f9b5d
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-06 17:13:45 +0000

    Updated StasticsOrchestra

--- src/SFA.DAS.EmployerFinance.Api/Controllers/StatisticsController.cs
+++ src/SFA.DAS.EmployerFinance.Api/Controllers/StatisticsController.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Authorization.WebApi.Attributes;
+using SFA.DAS.EmployerFinance.Api.Orchestrators;
 using SFA.DAS.EmployerFinance.Queries.GetStatistics;
 
 namespace SFA.DAS.EmployerFinance.Api.Controllers
@@ -10,16 +11,18 @@ namespace SFA.DAS.EmployerFinance.Api.Controllers
     [Route("api/financestatistics")]
     public class StatisticsController : ControllerBase
     {
-        private readonly IMediator _mediator;
+        private readonly StatisticsOrchestrator _statisticsOrchestrator;
 
-        public StatisticsController(IMediator mediator) => _mediator = mediator;
+        public StatisticsController(StatisticsOrchestrator statisticsController)
+        {
+            _statisticsOrchestrator = statisticsController;
+        }
 
         [HttpGet]
         [Route("")]
         public async Task<IActionResult> GetStatistics()
         {
-            var response = await _mediator.Send(new GetStatisticsQuery());
-            return Ok(response.Statistics);
+            return Ok(await _statisticsOrchestrator.Get());
         }
     }
 }
