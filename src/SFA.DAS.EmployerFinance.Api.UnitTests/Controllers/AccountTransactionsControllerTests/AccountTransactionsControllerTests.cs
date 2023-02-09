5568c2725e9d9f43071529c1e9711a71e68d8376
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-09 13:51:31 +0000

    Added new class file

---
+++ src/SFA.DAS.EmployerFinance.Api.UnitTests/Controllers/AccountTransactionsControllerTests/AccountTransactionsControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerFinance.Api.UnitTests.Controllers.AccountTransactionsControllerTests
+{
+    internal class AccountTransactionsControllerTests
+    {
+    }
+}
