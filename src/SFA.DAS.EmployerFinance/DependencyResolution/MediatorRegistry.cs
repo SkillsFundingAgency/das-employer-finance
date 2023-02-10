b525300f4c6953af461cb9ff97fc2047d2356444
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-10 11:07:36 +0000

    Adding Mediator Registry

---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/MediatorRegistry.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using StructureMap;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class MediatorRegistry : Registry
+    {
+        public MediatorRegistry()
+        {
+            For<IMediator>().Use<Mediator>();
+        }
+    }
+}
