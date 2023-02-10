f4f70d689cb7582bc728d71020a6c595fe939c12
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-10 11:15:34 +0000

    Adding MessagePublisherRegistry

---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/MessagePublisherRegistry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.Messaging.AzureServiceBus.StructureMap;
+using SFA.DAS.Messaging.AzureServiceBus;
+using SFA.DAS.NLog.Logger;
+using StructureMap;
+using SFA.DAS.EmployerFinance.Configuration;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class MessagePublisherRegistry : Registry
+    {
+        public MessagePublisherRegistry()
+        {
+            Policies.Add(new TopicMessagePublisherPolicy<EmployerFinanceConfiguration>("SFA.DAS.EmployerFinance", "1.0", new NLogLogger(typeof(TopicMessagePublisher))));
+        }
+    }
+}
