9887871c68c4e92c8c90c24f56b5b249a9d1e578
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2023-02-10 15:24:01 +0000

    Adding ICookieService

---
+++ src/SFA.DAS.EmployerFinance/Interfaces/ICookieService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SFA.DAS.EmployerFinance.Interfaces
+{
+    public interface ICookieService<T>
+    {
+        void Create(IHttpContextAccessor contextAccessor, string name, T content, int expireDays);
+        void Update(IHttpContextAccessor contextAccessor, string name, T content);
+        void Delete(IHttpContextAccessor contextAccessor, string name);
+        T Get(IHttpContextAccessor contextAccessor, string name);
+    }
+}
