ec4a85987a31c1df8f793972c58716d24aa114b4
Jon Harris <jon.harris@digital.education.gov.uk>
2023-02-21 14:02:56 +0000

    Fixing failing test

--- src/SFA.DAS.EmployerFinance/Http/HttpResponseLogger.cs
+++ src/SFA.DAS.EmployerFinance/Http/HttpResponseLogger.cs
@@ -16,12 +16,8 @@ public class HttpResponseLogger : IHttpResponseLogger
         {
             var content = await response.Content.ReadAsStringAsync();
 
-            _logger.LogDebug("Logged response", new Dictionary<string, object>
-            {
-                { "StatusCode", response.StatusCode },
-                { "Reason", response.ReasonPhrase },
-                { "Content", content }
-            });
+            //_logger.LogDebug("Logged response. StatusCode: '{StatusCode}'. Reason: '{Reason}'. Content: '{Content}'", response.StatusCode, response.ReasonPhrase, content);
+            _logger.LogDebug("Logged response. StatusCode: '{StatusCode}'. Reason: '{Reason}'. Content: '{Content}'.", response.StatusCode, response.ReasonPhrase, content);
         }
     }
 
