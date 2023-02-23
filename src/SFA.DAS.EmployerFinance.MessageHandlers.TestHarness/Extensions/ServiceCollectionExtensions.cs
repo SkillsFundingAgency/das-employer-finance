266b2657295246adcbde74c60d307c64c0f832c7
Jon Harris <jon.harris@digital.education.gov.uk>
2023-02-23 14:56:18 +0000

    Refactor to remove StructureMap

---
+++ src/SFA.DAS.EmployerFinance.MessageHandlers.TestHarness/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using SFA.DAS.EmployerFinance.Configuration;
+using SFA.DAS.EmployerFinance.Extensions;
+using SFA.DAS.NServiceBus.Configuration;
+using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
+using SFA.DAS.NServiceBus.Hosting;
+using SFA.DAS.NServiceBus.SqlServer.Configuration;
+
+namespace SFA.DAS.EmployerFinance.MessageHandlers.TestHarness.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    private const string EndpointName = "SFA.DAS.EmployerFinance.MessageHandlers";
+
+    public static IServiceCollection AddNServiceBus(this IServiceCollection services)
+    {
+        return services
+            .AddSingleton(p =>
+            {
+                var employerFinanceConfiguration = p.GetService<EmployerFinanceConfiguration>();
+                var configuration = p.GetService<IConfiguration>();
+                var isLocal = configuration["EnvironmentName"].Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase);
+
+                var endpointConfiguration = new EndpointConfiguration(EndpointName)
+                    .UseErrorQueue($"{EndpointName}-errors")
+                    .UseInstallers()
+                    .UseOutbox()
+                    .UseLicense(employerFinanceConfiguration.NServiceBusLicense)
+                    .UseMessageConventions()
+                    .UseNewtonsoftJsonSerializer()
+                    .UseSqlServerPersistence(() => DatabaseExtensions.GetSqlConnection(employerFinanceConfiguration.DatabaseConnectionString))
+                    .UseAzureServiceBusTransport(() => employerFinanceConfiguration.ServiceBusConnectionString, isLocal);
+
+                var endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+                return endpoint;
+            })
+            .AddSingleton<IMessageSession>(s => s.GetService<IEndpointInstance>())
+            .AddHostedService<NServiceBusHostedService>();
+    }
+}
