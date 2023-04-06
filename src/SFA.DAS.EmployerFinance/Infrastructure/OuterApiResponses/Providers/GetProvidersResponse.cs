36913ad9b1ba4f48b7fe24ae8ab17b6199b93dbb
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-04-06 13:45:16 +0100

    remove unused outer api request and response

--- src/SFA.DAS.EmployerFinance/Infrastructure/OuterApiResponses/Providers/GetProvidersResponse.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace SFA.DAS.EmployerFinance.Infrastructure.OuterApiResponses.Providers;
-
-public class GetProvidersResponse
-{
-    [JsonProperty("providers")]
-    public List<ProviderResponse> Providers { get; set; }
-}
