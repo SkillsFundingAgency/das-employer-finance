e8607926d94f993f8439fb20bce40be4a570b4dc
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-05-09 16:59:37 +0100

    add nservicebus unit of work reg

--- src/SFA.DAS.EmployerFinance/ServiceRegistration/EventsApiServiceRegistrations.cs
+++ src/SFA.DAS.EmployerFinance/ServiceRegistration/EventsApiServiceRegistrations.cs
@@ -1,5 +1,6 @@
 ﻿using SFA.DAS.Events.Api.Client;
 using SFA.DAS.Events.Api.Client.Configuration;
+using SFA.DAS.UnitOfWork.Pipeline;
 
 namespace SFA.DAS.EmployerFinance.ServiceRegistration;
 
@@ -7,6 +8,7 @@ public static class EventsApiServiceRegistrations
 {
     public static IServiceCollection AddEventsApi(this IServiceCollection services)
     {
+        services.AddScoped<IUnitOfWork, UnitOfWork.NServiceBus.Pipeline.UnitOfWork>();
         services.AddTransient<IEventsApi>(s =>
         {
             var config = s.GetService<IEventsApiClientConfiguration>();
