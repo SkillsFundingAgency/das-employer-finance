3c9caba6fc9c6d9f6d41a01a904325ce93721331
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-06-12 16:15:09 +0100

    remove duplicate

--- src/SFA.DAS.EmployerFinance.Web/Startup.cs
+++ src/SFA.DAS.EmployerFinance.Web/Startup.cs
@@ -105,7 +105,6 @@ public class Startup
 
         });
 
-        services.AddNServiceBusClientUnitOfWork();
         services
             .AddUnitOfWork()
             .AddEntityFramework(employerFinanceWebConfiguration)
