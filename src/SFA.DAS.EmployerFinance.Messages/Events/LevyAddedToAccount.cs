70a28fe623af1e9d354a831c97964d58cac7008e
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-06-19 15:47:57 +0100

    remove sfa.das.nservicebus for events

--- src/SFA.DAS.EmployerFinance.Messages/Events/LevyAddedToAccount.cs
+++ src/SFA.DAS.EmployerFinance.Messages/Events/LevyAddedToAccount.cs
@@ -1,8 +1,6 @@
-﻿using SFA.DAS.NServiceBus;
-
-namespace SFA.DAS.EmployerFinance.Messages.Events
+﻿namespace SFA.DAS.EmployerFinance.Messages.Events
 {
-    public class LevyAddedToAccount : Event
+    public class LevyAddedToAccount
     {
         public long AccountId { get; set; }
         public decimal Amount { get; set; }
