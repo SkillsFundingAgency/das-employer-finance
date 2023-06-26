08ec34707745fc026ca069418a853592de0fc173
ogrwill <will.ogrady@digital.education.gov.uk>
2023-06-26 12:24:46 +0100

    MAP-283 applying logic to create pledge button

--- src/SFA.DAS.EmployerFinance.Web/wwwroot/js/app.js
+++ src/SFA.DAS.EmployerFinance.Web/wwwroot/js/app.js
@@ -5,3 +5,12 @@ Array.prototype.forEach.call(limitCharsInNumberFields, function forElement(field
         e.target.value=e.target.value.slice(0,e.target.maxLength) 
     })
 });
+
+var createTransfersPledgeButton = document.getElementById("CreateTransfersPledgeButton");
+
+// Disable click action on the link if it is disabled
+if (createTransfersPledgeButton.getAttribute("disabled")) {
+    createTransfersPledgeButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent the default action of the click event
+    });
+}
