cfa0c97a8ed9e68fb0d1122d8eb50d100f1cd7ee
Jon Harris <jon.harris@digital.education.gov.uk>
2023-07-20 07:56:54 +0100

    Set log level to info for appinsights

--- src/SFA.DAS.EmployerFinance.Web/Startup.cs
+++ src/SFA.DAS.EmployerFinance.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 using NServiceBus.ObjectBuilder.MSDependencyInjection;
 using SFA.DAS.Employer.Shared.UI;
 using SFA.DAS.EmployerFinance.Configuration;
@@ -40,7 +41,11 @@ public class Startup
 
         services.AddOptions();
 
-        services.AddLogging();
+        services.AddLogging(builder =>
+        {
+            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+        });
 
         services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
 
