f331bfedf2cc96e030e926bc4e49650f4a0ad8ec
Jon Harris <jon.harris@digital.education.gov.uk>
2023-08-15 15:55:09 +0100

    Test fix

--- src/SFA.DAS.EmployerFinance.UnitTests/Formatters/Transactions/ExcelTransactionFormatterTests/WhenICreateAExcelFile.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Formatters/Transactions/ExcelTransactionFormatterTests/WhenICreateAExcelFile.cs
@@ -53,7 +53,7 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Formatters.Transactions.ExcelTransac
                 },
                 new[]
                 {
-                    _transactionLine.DateCreated.ToString(), _transactionLine.TransactionType, _transactionLine.Description,
+                    _transactionLine.DateCreated.ToString("dd/MM/yyyy"), _transactionLine.TransactionType, _transactionLine.Description,
                     _transactionLine.PayeScheme, "'" +  _transactionLine.PeriodEnd, _transactionLine.LevyDeclaredFormatted,
                     _transactionLine.EnglishFractionFormatted, _transactionLine.TenPercentTopUpFormatted,
                     _transactionLine.TrainingProvider, _transactionLine.Uln,
