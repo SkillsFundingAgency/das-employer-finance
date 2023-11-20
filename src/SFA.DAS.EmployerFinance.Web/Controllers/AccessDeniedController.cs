756a2b5e2b8e607e161970b52ff11b4cba76333f
Deven Shah <devenshah1974@gmail.com>
2023-11-20 11:42:45 +0000

    CSP-1114 Fixed failing to navigate to access denied page

--- src/SFA.DAS.EmployerFinance.Web/Controllers/AccessDeniedController.cs
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/AccessDeniedController.cs
@@ -1,9 +1,13 @@
-﻿namespace SFA.DAS.EmployerFinance.Web.Controllers;
-
-    public class AccessDeniedController : Controller
+﻿namespace SFA.DAS.EmployerFinance.Web.Controllers
 {
+    [Route("[Controller]")]
+    [Route("error/403")]
+    public class AccessDeniedController : Controller
+    {
         public IActionResult Index()
         {
+            Response.StatusCode = StatusCodes.Status401Unauthorized;
             return View();
         }
     }
+}
