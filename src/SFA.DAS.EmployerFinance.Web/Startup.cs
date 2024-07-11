38bca5d6a81c19904b071d7300f3493408ea836a
Paul Graham <paul.graham@coprime.co.uk>
2024-07-11 14:00:31 +0100

    add action filter so account Id gets populated

--- src/SFA.DAS.EmployerFinance.Web/Startup.cs
+++ src/SFA.DAS.EmployerFinance.Web/Startup.cs
@@ -106,8 +106,8 @@ public class Startup
             if (!_configuration.IsDev())
             {
                 options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+                options.Filters.Add(new ZendeskApiFilterAttribute());
             }
-
         });
 
         services
