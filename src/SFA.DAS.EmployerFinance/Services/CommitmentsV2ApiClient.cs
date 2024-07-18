7d683e1f71a14626927f07cabc699593094c0d16
Jon Harris <jon.harris@digital.education.gov.uk>
2024-07-18 07:28:25 +0100

    Preventing access token generation for each call.

--- src/SFA.DAS.EmployerFinance/Services/CommitmentsV2ApiClient.cs
+++ src/SFA.DAS.EmployerFinance/Services/CommitmentsV2ApiClient.cs
@@ -1,69 +1,82 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.Json;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 using SFA.DAS.CommitmentsV2.Api.Types.Responses;
 using SFA.DAS.EmployerFinance.Configuration;
 using SFA.DAS.EmployerFinance.Interfaces;
 
 namespace SFA.DAS.EmployerFinance.Services;
 
-public class CommitmentsV2ApiClient(
-    HttpClient httpClient,
-    CommitmentsApiV2ClientConfiguration commitmentsApiClientConfig,
-    ILogger<CommitmentsV2ApiClient> logger,
-    IConfiguration configuration)
-    : ICommitmentsV2ApiClient
+public class CommitmentsV2ApiClient : ICommitmentsV2ApiClient
 {
+    private readonly HttpClient _httpClient;
+    private readonly CommitmentsApiV2ClientConfiguration _commitmentsApiClientConfig;
+    private readonly ILogger<CommitmentsV2ApiClient> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly AzureServiceTokenProvider _azureServiceTokenProvider;
+
+    public CommitmentsV2ApiClient(HttpClient httpClient,
+        CommitmentsApiV2ClientConfiguration commitmentsApiClientConfig,
+        ILogger<CommitmentsV2ApiClient> logger,
+        IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _commitmentsApiClientConfig = commitmentsApiClientConfig;
+        _logger = logger;
+        _configuration = configuration;
+        
+        _azureServiceTokenProvider = new AzureServiceTokenProvider();
+    }
+
     public async Task<GetApprenticeshipResponse> GetApprenticeship(long apprenticeshipId)
     {
         var url = $"{BaseUrl()}api/apprenticeships/{apprenticeshipId}";
-        logger.LogInformation("EmployerFinance Services Getting GetApprenticeship {Url}", url);
+        _logger.LogInformation("EmployerFinance Services Getting GetApprenticeship {Url}", url);
 
         using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
         await AddAuthenticationHeader(requestMessage);
-      
-        var response = await httpClient.SendAsync(requestMessage).ConfigureAwait(false);
+
+        var response = await _httpClient.SendAsync(requestMessage).ConfigureAwait(false);
         var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-        logger.LogInformation("EmployerFinance Services received response for GetApprenticeship {Url}", url);
+        _logger.LogInformation("EmployerFinance Services received response for GetApprenticeship {Url}", url);
 
-        return JsonConvert.DeserializeObject<GetApprenticeshipResponse>(json);
+        return JsonSerializer.Deserialize<GetApprenticeshipResponse>(json);
     }
 
     public async Task<GetTransferRequestSummaryResponse> GetTransferRequests(long accountId)
     {
         var url = $"{BaseUrl()}api/accounts/{accountId}/transfers";
-        logger.LogInformation("Getting GetTransferRequests {Url}", url);
-        
+        _logger.LogInformation("Getting GetTransferRequests {Url}", url);
+
         using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
         await AddAuthenticationHeader(requestMessage);
 
-        var response = await httpClient.SendAsync(requestMessage).ConfigureAwait(false);
+        var response = await _httpClient.SendAsync(requestMessage).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
 
         var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-        
-        return JsonConvert.DeserializeObject<GetTransferRequestSummaryResponse>(json);
+
+        return JsonSerializer.Deserialize<GetTransferRequestSummaryResponse>(json);
     }
 
     private string BaseUrl()
     {
-        if (commitmentsApiClientConfig.ApiBaseUrl.EndsWith("/"))
+        if (_commitmentsApiClientConfig.ApiBaseUrl.EndsWith("/"))
         {
-            return commitmentsApiClientConfig.ApiBaseUrl;
+            return _commitmentsApiClientConfig.ApiBaseUrl;
         }
 
-        return commitmentsApiClientConfig.ApiBaseUrl + "/";
+        return _commitmentsApiClientConfig.ApiBaseUrl + "/";
     }
 
     private async Task AddAuthenticationHeader(HttpRequestMessage httpRequestMessage)
     {
-        if (configuration["EnvironmentName"].ToUpper() != "LOCAL")
+        if (!_configuration["EnvironmentName"].Equals("LOCAL", StringComparison.OrdinalIgnoreCase))
         {
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var accessToken = await azureServiceTokenProvider.GetAccessTokenAsync(commitmentsApiClientConfig.IdentifierUri);
+            var accessToken = await _azureServiceTokenProvider.GetAccessTokenAsync(_commitmentsApiClientConfig.IdentifierUri);
             httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
         }
     }
