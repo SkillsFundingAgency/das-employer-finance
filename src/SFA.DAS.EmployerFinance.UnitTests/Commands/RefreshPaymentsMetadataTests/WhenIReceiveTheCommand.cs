737fb9babdbc44d50d70a4fc199feade24ec94b7
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-01 08:10:21 +0100

    Adding tests

--- src/SFA.DAS.EmployerFinance.UnitTests/Commands/RefreshPaymentsMetadataTests/WhenIReceiveTheCommand.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Commands/RefreshPaymentsMetadataTests/WhenIReceiveTheCommand.cs
@@ -1,6 +1,39 @@
-﻿namespace SFA.DAS.EmployerFinance.UnitTests.Commands.RefreshPaymentsMetadataTests;
+﻿using System.Security.Cryptography.X509Certificates;
+using AutoFixture.NUnit3;
+using Microsoft.EntityFrameworkCore.Internal;
+using SFA.DAS.EmployerFinance.Commands.RefreshPaymentMetadata;
+using SFA.DAS.EmployerFinance.Data;
+using SFA.DAS.EmployerFinance.Data.Contracts;
+using SFA.DAS.EmployerFinance.Models.Payments;
+using SFA.DAS.EmployerFinance.Services.Contracts;
+using SFA.DAS.EmployerFinance.TestCommon.DatabaseMock;
+using SFA.DAS.EmployerFinance.Validation;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerFinance.UnitTests.Commands.RefreshPaymentsMetadataTests;
 
 public class WhenIReceiveTheCommand
 {
-    
+    // [Test, MoqAutoData]
+    // public async Task ThenTheCommandIsValidated(
+    //     [Frozen]Mock<IValidator<RefreshPaymentMetadataCommand>> validator,
+    //     RefreshPaymentMetadataCommand command,
+    //     RefreshPaymentMetadataCommandHandler handler,
+    //     [Frozen]Mock<EmployerFinanceDbContext> dbContext,
+    //     IList<Payment> payments)
+    // {
+    //     validator.Setup(x =>
+    //         x.Validate(It.Is<RefreshPaymentMetadataCommand>(c => 
+    //             c.PaymentId == command.PaymentId 
+    //             && c.AccountId == command.AccountId
+    //             && c.PeriodEndRef == command.PeriodEndRef)))
+    //         .Returns(new ValidationResult { ValidationDictionary = new Dictionary<string, string>() })
+    //         .Verifiable();
+    //
+    //     dbContext.Setup(x => x.Payments).ReturnsDbSet(payments);
+    //
+    //     await handler.Handle(command, CancellationToken.None);
+    //
+    //     validator.Verify();
+    // }
 }
--- src/SFA.DAS.EmployerFinance.UnitTests/Commands/RefreshPaymentsMetadataTests/WhenIValidateTheCommand.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Commands/RefreshPaymentsMetadataTests/WhenIValidateTheCommand.cs
@@ -1,6 +1,41 @@
-﻿namespace SFA.DAS.EmployerFinance.UnitTests.Commands.RefreshPaymentsMetadataTests;
+﻿using AutoFixture.NUnit3;
+using SFA.DAS.EmployerFinance.Commands.RefreshPaymentMetadata;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerFinance.UnitTests.Commands.RefreshPaymentsMetadataTests;
 
 public class WhenIValidateTheCommand
 {
+    [Test, MoqAutoData]
+    public void ThenTrueIsReturnedWhenAllFieldsAreValid(
+        RefreshPaymentMetadataCommand command,
+        RefreshPaymentMetadataCommandValidator validator
+        )
+    {
+        var actual = validator.Validate(command);
+
+        actual.IsValid().Should().BeTrue();
+    }
     
+    [Test, MoqAutoData]
+    public void ThenFalseIsReturnedWhenFieldsAreInValid(
+        RefreshPaymentMetadataCommandValidator validator
+    )
+    {
+        var actual = validator.Validate(new RefreshPaymentMetadataCommand());
+
+        actual.IsValid().Should().BeFalse();
+    }
+    
+    [Test, MoqAutoData]
+    public void ThenFalseIsReturnedWhenAllFieldsArentPopulatedAndTheErrorDictionaryIsPopulated(
+        RefreshPaymentMetadataCommandValidator validator
+    )
+    {
+        var actual = validator.Validate(new RefreshPaymentMetadataCommand());
+
+        actual.IsValid().Should().BeFalse();
+        actual.ValidationDictionary.Should().Contain(new KeyValuePair<string, string>("AccountId", "AccountId has not been supplied"));
+        actual.ValidationDictionary.Should().Contain(new KeyValuePair<string, string>("PaymentId", "PaymentId has not been supplied"));
+    }
 }
