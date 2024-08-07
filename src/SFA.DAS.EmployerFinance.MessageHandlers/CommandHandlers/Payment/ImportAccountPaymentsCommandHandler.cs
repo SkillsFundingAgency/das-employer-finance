8f84cb509b0caf72764c5caf23d14fea6fbc29fb
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-07 12:19:00 +0100

    Refactor

--- src/SFA.DAS.EmployerFinance.MessageHandlers/CommandHandlers/Payment/ImportAccountPaymentsCommandHandler.cs
+++ src/SFA.DAS.EmployerFinance.MessageHandlers/CommandHandlers/Payment/ImportAccountPaymentsCommandHandler.cs
@@ -2,6 +2,7 @@
 using SFA.DAS.EmployerFinance.Commands.RefreshAccountTransfers;
 using SFA.DAS.EmployerFinance.Commands.RefreshPaymentData;
 using SFA.DAS.EmployerFinance.Messages.Commands;
+using SFA.DAS.EmployerFinance.Models.Payments;
 
 namespace SFA.DAS.EmployerFinance.MessageHandlers.CommandHandlers.Payment;
 
@@ -14,24 +15,47 @@ public class ImportAccountPaymentsCommandHandler(
     {
         var correlationId = Guid.NewGuid();
 
-        logger.LogInformation($"Processing refresh payment command for Account ID: {message.AccountId} PeriodEnd: {message.PeriodEndRef} CorrelationId: {correlationId}, MessageId: {context.MessageId}");
+        var paymentsResponse = await RefreshPaymentData(message, context, correlationId);
 
-        var paymentsResponse = await mediator.Send(new RefreshPaymentDataCommand
-        {
-            AccountId = message.AccountId,
-            PeriodEnd = message.PeriodEndRef,
-            CorrelationId = correlationId
-        }).ConfigureAwait(false);
+        await RefreshAccountTransfers(message, correlationId);
 
-        logger.LogInformation($"Processing refresh account transfers command for AccountId:{message.AccountId} PeriodEnd:{message.PeriodEndRef}, CorrelationId: {correlationId}");
+        await CreateTransferTransactions(message, correlationId);
 
-        await mediator.Send(new RefreshAccountTransfersCommand
-        {
-            ReceiverAccountId = message.AccountId,
-            PeriodEnd = message.PeriodEndRef,
-            CorrelationId = correlationId
-        }).ConfigureAwait(false);
+        await ImportAccountPaymentMetadata(message, context, paymentsResponse);
+    }
 
+    private async Task ImportAccountPaymentMetadata(ImportAccountPaymentsCommand message, IMessageHandlerContext context, RefreshPaymentDataResponse paymentsResponse)
+    {
+        await Parallel.ForEachAsync(paymentsResponse.PaymentDetails,
+            async (payment, _) =>
+            {
+                await SendImportAccountPaymentMetadataCommand(message, context, payment);
+            });
+    }
+
+    private async Task SendImportAccountPaymentMetadataCommand(ImportAccountPaymentsCommand message, IMessageHandlerContext context, PaymentDetails payment)
+    {
+        logger.LogInformation(
+            "Creating {MessageType} message for {AccountId} - {PaymentId}",
+            nameof(ImportAccountPaymentMetadataCommand),
+            message.AccountId,
+            payment.Id);
+
+        var sendOptions = new SendOptions();
+        sendOptions.RouteToThisEndpoint();
+        sendOptions.SetMessageId($"{nameof(ImportAccountPaymentsCommand)}-{message.PeriodEndRef}-{message.AccountId}-{payment.Id}");
+
+        await context
+            .Send(new ImportAccountPaymentMetadataCommand
+            {
+                AccountId = message.AccountId,
+                PaymentId = payment.Id,
+                PeriodEndRef = payment.PeriodEnd
+            }, sendOptions);
+    }
+
+    private async Task CreateTransferTransactions(ImportAccountPaymentsCommand message, Guid correlationId)
+    {
         logger.LogInformation($"Processing create account transfer transactions command for AccountId:{message.AccountId} PeriodEnd:{message.PeriodEndRef}, CorrelationId: {correlationId}");
 
         await mediator.Send(new CreateTransferTransactionsCommand
@@ -40,29 +64,29 @@ public class ImportAccountPaymentsCommandHandler(
             PeriodEnd = message.PeriodEndRef,
             CorrelationId = correlationId
         }).ConfigureAwait(false);
+    }
 
-        await Parallel.ForEachAsync(paymentsResponse.PaymentDetails,
-            async (payment, _) =>
-            {
-                logger.LogInformation(
-                    "Creating {MessageType} message for {AccountId} - {PaymentId}",
-                    nameof(ImportAccountPaymentMetadataCommand),
-                    message.AccountId,
-                    payment.Id);
+    private async Task RefreshAccountTransfers(ImportAccountPaymentsCommand message, Guid correlationId)
+    {
+        logger.LogInformation($"Processing refresh account transfers command for AccountId:{message.AccountId} PeriodEnd:{message.PeriodEndRef}, CorrelationId: {correlationId}");
 
-                var sendOptions = new SendOptions();
-                sendOptions.RouteToThisEndpoint();
-                sendOptions.SetMessageId(
-                    $"{nameof(ImportAccountPaymentsCommand)}-{message.PeriodEndRef}-{message.AccountId}-{payment.Id}");
+        await mediator.Send(new RefreshAccountTransfersCommand
+        {
+            ReceiverAccountId = message.AccountId,
+            PeriodEnd = message.PeriodEndRef,
+            CorrelationId = correlationId
+        }).ConfigureAwait(false);
+    }
 
-                await context
-                    .Send(new ImportAccountPaymentMetadataCommand
-                    {
-                        AccountId = message.AccountId,
-                        PaymentId = payment.Id,
-                        PeriodEndRef = payment.PeriodEnd
-                    }, sendOptions)
-                    .ConfigureAwait(false);
-            }).ConfigureAwait(false);
+    private async Task<RefreshPaymentDataResponse> RefreshPaymentData(ImportAccountPaymentsCommand message, IMessageHandlerContext context, Guid correlationId)
+    {
+        logger.LogInformation($"Processing refresh payment command for Account ID: {message.AccountId} PeriodEnd: {message.PeriodEndRef} CorrelationId: {correlationId}, MessageId: {context.MessageId}");
+
+        return await mediator.Send(new RefreshPaymentDataCommand
+        {
+            AccountId = message.AccountId,
+            PeriodEnd = message.PeriodEndRef,
+            CorrelationId = correlationId
+        }).ConfigureAwait(false);
     }
 }
