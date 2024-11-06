5015d888bcd5f907ccccb0275868bc71e84f369b
Paul Graham <paul.graham@coprime.co.uk>
2024-11-06 12:09:58 +0000

    added datecreated to all events

--- src/SFA.DAS.EmployerFinance.Messages/Events/LevyAddedToAccount.cs
+++ src/SFA.DAS.EmployerFinance.Messages/Events/LevyAddedToAccount.cs
@@ -1,8 +1,11 @@
-﻿namespace SFA.DAS.EmployerFinance.Messages.Events
+﻿using System;
+
+namespace SFA.DAS.EmployerFinance.Messages.Events
 {
     public class LevyAddedToAccount
     {
         public long AccountId { get; set; }
         public decimal Amount { get; set; }
+        public DateTime Created { get; set; }
     }
 }
