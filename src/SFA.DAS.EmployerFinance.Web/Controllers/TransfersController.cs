b435d5dde5a9c865c0bb49e6e6d504da87789409
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2024-11-13 21:27:01 +0000

    inspect user

--- src/SFA.DAS.EmployerFinance.Web/Controllers/TransfersController.cs
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/TransfersController.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.Employer.Shared.UI;
+﻿using Newtonsoft.Json;
+using SFA.DAS.Employer.Shared.UI;
 using SFA.DAS.Employer.Shared.UI.Attributes;
 using SFA.DAS.EmployerFinance.Web.Authentication;
 using SFA.DAS.EmployerFinance.Web.Orchestrators;
@@ -8,20 +9,16 @@ namespace SFA.DAS.EmployerFinance.Web.Controllers;
 [SetNavigationSection(NavigationSection.AccountsFinance)]
 [Authorize(Policy = nameof(PolicyNames.HasEmployerViewerTransactorOwnerAccount))]
 [Route("accounts/{HashedAccountId}")] 
-public class TransfersController :Controller
+public class TransfersController(
+    TransfersOrchestrator transfersOrchestrator,
+    ILogger<TransfersController> logger) : Controller
 {
-    private readonly TransfersOrchestrator _transfersOrchestrator;
-
-    public TransfersController(TransfersOrchestrator transfersOrchestrator)
-    {
-        _transfersOrchestrator = transfersOrchestrator;
-    }
-
     [HttpGet]
     [Route("transfers")]
     public async Task<IActionResult> Index(string hashedAccountId)
     {
-        var viewModel = await _transfersOrchestrator.GetIndexViewModel(hashedAccountId);
+        logger.LogInformation("CF: Index: {ContextUser}", JsonConvert.SerializeObject(User));
+        var viewModel = await transfersOrchestrator.GetIndexViewModel(hashedAccountId);
 
         return View(viewModel);
     }
@@ -30,7 +27,7 @@ public class TransfersController :Controller
     [Route("transfers/financial-breakdown")]
     public async Task<IActionResult> FinancialBreakdown(string hashedAccountId)
     {
-        var viewModel = await _transfersOrchestrator.GetFinancialBreakdownViewModel(hashedAccountId);
+        var viewModel = await transfersOrchestrator.GetFinancialBreakdownViewModel(hashedAccountId);
         return View(viewModel);
     }
 }
