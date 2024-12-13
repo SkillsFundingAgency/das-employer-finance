f88f15adb3d18b569cfa0483863d78b1ca04d078
Paul Graham <paul.graham@coprime.co.uk>
2024-12-13 13:01:41 +0000

    removed Event base class

--- src/SFA.DAS.EmployerFinance.Messages/Events/SentTransferConnectionRequestEvent.cs
+++ src/SFA.DAS.EmployerFinance.Messages/Events/SentTransferConnectionRequestEvent.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using SFA.DAS.NServiceBus;
 
 namespace SFA.DAS.EmployerFinance.Messages.Events
 {
-    public class SentTransferConnectionRequestEvent : Event
+    public class SentTransferConnectionRequestEvent
     {
         public long ReceiverAccountId { get; set; }
         public string ReceiverAccountName { get; set; }
@@ -13,5 +12,6 @@ namespace SFA.DAS.EmployerFinance.Messages.Events
         public string SentByUserName { get; set; }
         public Guid SentByUserRef { get; set; }
         public int TransferConnectionRequestId { get; set; }
+        public DateTime Created { get; set; }
     }
 }
