aaaadcdf5c837031fd5cdf3d0a14cacfc806d6ea
Paul Graham <paul.graham@coprime.co.uk>
2024-12-18 11:36:52 +0000

    try this fix

---
+++ src/SFA.DAS.EmployerFinance.MessageHandlers/LevyAddedToAccount.cs
@@ -0,0 +1,24 @@
+﻿using SFA.DAS.NServiceBus;
+using SFA.DAS.NServiceBus.Services;
+
+namespace SFA.DAS.EmployerFinance.Messages.Events
+{
+
+    public static class LevyAddedToAccountWithEventBaseClass
+    {
+        public static async Task Publish(IEventPublisher eventPublisher, decimal levyTotalTransactionValue, long accountId)
+        {
+            await eventPublisher.Publish(new LevyAddedToAccount
+            {
+                AccountId = accountId,
+                Amount = levyTotalTransactionValue
+            });
+        }
+    }
+
+    public class LevyAddedToAccount : Event
+    {
+        public long AccountId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ src/SFA.DAS.EmployerFinance/Commands/RefreshEmployerLevyData/LevyAddedToAccountWithEventBaseClass.cs
@@ -0,0 +1,25 @@
+﻿using SFA.DAS.NServiceBus;
+using SFA.DAS.NServiceBus.Services;
+
+// This is a hack to ensure we can create a LevyAddedToAccount with Event as a base class, the class is only published in this project and never consumed here
+// The idea is to create an identical class in the Messages project which has no dependencies on Event and SFA.DAS.NServiceBus. But the namespace must match.  
+namespace SFA.DAS.EmployerFinance.Messages.Events
+{
+    public static class LevyAddedToAccountWithEventBaseClass
+    {
+        public static async Task Publish(IEventPublisher eventPublisher, decimal levyTotalTransactionValue, long accountId)
+        {
+            await eventPublisher.Publish(new LevyAddedToAccount
+            {
+                AccountId = accountId,
+                Amount = levyTotalTransactionValue
+            });
+        }
+    }
+
+    public class LevyAddedToAccount : Event
+    {
+        public long AccountId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
