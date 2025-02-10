92b299829bc496e96af4880b503ea2d9759fe47a
Jon Harris <jon.harris@fuitsu.com>
2025-02-10 10:31:35 +0000

    Web security headers improvements

--- src/SFA.DAS.EmployerFinance.Web.UnitTests/Middleware/SecurityHeadersMiddlewareTests.cs
+++ src/SFA.DAS.EmployerFinance.Web.UnitTests/Middleware/SecurityHeadersMiddlewareTests.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.EmployerFinance.Web.Middleware;
+﻿using Microsoft.Extensions.Primitives;
+using SFA.DAS.EmployerFinance.Web.Middleware;
 
 namespace SFA.DAS.EmployerFinance.Web.UnitTests.Middleware;
 
@@ -7,6 +8,7 @@ public class SecurityHeadersMiddlewareTests
 {
     private Mock<RequestDelegate> _nextMock;
     private SecurityHeadersMiddleware _middleware;
+    private const string DasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net";
     
     [SetUp]
     public void Setup()
@@ -20,16 +22,23 @@ public class SecurityHeadersMiddlewareTests
     {
         // Arrange
         var context = new DefaultHttpContext();
-        
+        var cspValues = new StringValues(
+            $"default-src *; " +
+            $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {DasCdn} " +
+            "*.googletagmanager.com *.postcodeanywhere.co.uk *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com https://*.rcrsv.io;" +
+            "connect-src *; " +
+            "img-src *; " +
+            $"style-src 'self' 'unsafe-inline' {DasCdn} https://tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io ; " +
+            "object-src *;");
         // Act
         await _middleware.InvokeAsync(context);
 
         // Assert
         context.Response.Headers.XFrameOptions.Should().BeEquivalentTo("DENY");
         context.Response.Headers.XContentTypeOptions.Should().BeEquivalentTo("nosniff");
+        context.Response.Headers.ContentSecurityPolicy.Should().BeEquivalentTo(cspValues);
         context.Response.Headers.XFrameOptions.Should().BeEquivalentTo("DENY");
         context.Response.Headers["X-Permitted-Cross-Domain-Policies"].Should().BeEquivalentTo("none");
-        context.Response.Headers.ContentSecurityPolicy.Should().BeEquivalentTo("default-src *; script-src *; connect-src *; img-src *; style-src *; object-src *;");
 
         _nextMock.Verify(next => next(context), Times.Once);
     }
--- src/SFA.DAS.EmployerFinance.Web/Middleware/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.EmployerFinance.Web/Middleware/SecurityHeadersMiddleware.cs
@@ -7,11 +7,22 @@ public class SecurityHeadersMiddleware(RequestDelegate next)
 {
     public async Task InvokeAsync(HttpContext context)
     {
+        const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net";
+
         context.Response.Headers.AddIfNotPresent("x-frame-options", new StringValues("DENY"));
         context.Response.Headers.AddIfNotPresent("x-content-type-options", new StringValues("nosniff"));
         context.Response.Headers.AddIfNotPresent("X-Permitted-Cross-Domain-Policies", new StringValues("none"));
-        context.Response.Headers.AddIfNotPresent("Content-Security-Policy", new StringValues("default-src *; script-src *; connect-src *; img-src *; style-src *; object-src *;"));
-        
+        context.Response.Headers.AddIfNotPresent("x-xss-protection", new StringValues("0"));
+        context.Response.Headers.AddIfNotPresent("Content-Security-Policy", 
+            new StringValues(
+                $"default-src *; " +
+                $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} " +
+                "*.googletagmanager.com *.postcodeanywhere.co.uk *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com https://*.rcrsv.io;" +
+                "connect-src *; " +
+                "img-src *; " +
+                $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io ; " +
+                "object-src *;"));
+
         await next(context);
     }
 }
