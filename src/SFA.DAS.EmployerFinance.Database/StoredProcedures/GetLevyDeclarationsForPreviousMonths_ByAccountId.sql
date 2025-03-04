18aa477bc7672208ab9aea7a5f0fdea37c6c55f6
Jon Harris <jon.harris@fuitsu.com>
2025-03-04 15:10:29 +0000

    SP fix

--- src/SFA.DAS.EmployerFinance.Database/StoredProcedures/GetLevyDeclarationsForPreviousMonths_ByAccountId.sql
+++ src/SFA.DAS.EmployerFinance.Database/StoredProcedures/GetLevyDeclarationsForPreviousMonths_ByAccountId.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE [employer_financial].[GetLevyDeclarationsForPreviousMonths_ByAccountId]
-@AccountId bigint = 0;
-@months int;
+    @AccountId bigint = 0,
+    @months int
+    
 AS
 SET NOCOUNT ON;
 
@@ -10,11 +11,11 @@ FROM [employer_financial].[GetLevyDeclarationAndTopUp]
 WHERE EmpRef IN
 (
     SELECT
-    EmpRef
+        EmpRef
     FROM [employer_financial].LevyDeclaration
     WHERE AccountId = @AccountId
 )
 AND (LastSubmission = 1 OR EndOfYearAdjustment = 1)
 AND AccountId = @AccountId
-AND SubmissionDate > DATEADD(month, -@months, GETDATE())
+AND SubmissionDate >= DATEADD(month, -@months, GETDATE())
 ORDER BY SubmissionDate DESC
