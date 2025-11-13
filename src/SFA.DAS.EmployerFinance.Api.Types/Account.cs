6b50131aa63d1575029050b1dac97c6415e16b6c
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2025-11-13 10:11:16 +0000

    wip

--- src/SFA.DAS.EmployerFinance.Api.Types/Account.cs
+++ src/SFA.DAS.EmployerFinance.Api.Types/Account.cs
@@ -1,12 +1,14 @@
-﻿namespace SFA.DAS.EmployerFinance.Api.Types;
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.EmployerFinance.Api.Types;
 
 public class Account
 {
     public virtual long Id { get; set; }
-  //  public List<AccountLegalEntity> AccountLegalEntities { get; set; } = new List<AccountLegalEntity>();
+    public List<AccountLegalEntity> AccountLegalEntities { get; set; } = new List<AccountLegalEntity>();
     public virtual string Name { get; set; }
     public string HashedId { get; set; }
     public string PublicHashedId { get; set; }
-   // public List<TransferConnectionInvitation> ReceivedTransferConnectionInvitations { get; set; } = new List<TransferConnectionInvitation>();
- //   public List<TransferConnectionInvitation> SentTransferConnectionInvitations { get; set; } = new List<TransferConnectionInvitation>();
+    public List<TransferConnectionInvitation> ReceivedTransferConnectionInvitations { get; set; } = new List<TransferConnectionInvitation>();
+    public List<TransferConnectionInvitation> SentTransferConnectionInvitations { get; set; } = new List<TransferConnectionInvitation>();
 }
