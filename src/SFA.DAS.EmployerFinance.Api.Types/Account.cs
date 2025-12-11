239a7f7bf1814df9e6490b1770ad27181b05379c
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2025-12-11 14:57:54 +0000

    removed accountTy[e

--- src/SFA.DAS.EmployerFinance.Api.Types/Account.cs
+++ src/SFA.DAS.EmployerFinance.Api.Types/Account.cs
@@ -6,6 +6,5 @@ public class Account
 {
     public long Id { get; protected set; }
     public string Name { get; protected set; }
-    public string AccountType { get; protected set; }
     public DateTime CreatedDate { get; protected set; }
 }
