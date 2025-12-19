050662d15cfdc8f2337ca94c07c982905168417c
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2025-12-19 00:10:00 +0000

    some changes

--- src/SFA.DAS.EmployerFinance.Api/Controllers/TransferConnectionsController.cs
+++ src/SFA.DAS.EmployerFinance.Api/Controllers/TransferConnectionsController.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using SFA.DAS.EmployerFinance.Api.Authorization;
 using SFA.DAS.EmployerFinance.Models.TransferConnections;
 using SFA.DAS.EmployerFinance.Queries.GetTransferConnections;
 using SFA.DAS.EmployerFinance.Queries.GetTransfersbyPeriodEnd;
 using SFA.DAS.Encoding;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerFinance.Api.Controllers;
 
@@ -37,14 +38,30 @@ public class TransferConnectionsController(IMediator mediator, IEncodingService 
 
     [HttpGet]
     [Route("{accountId}/transfers/connectionss")]
-    public async Task<IActionResult> GetTransfersByPeriodEnd(long accountId, [FromQuery] string periodEnd)
+    public async Task<IActionResult> GetTransfersByPeriodEnd(long accountId,[FromQuery] string periodEnd)
     {
+        if (string.IsNullOrWhiteSpace(periodEnd))
+        {
+            return BadRequest("periodEnd query parameter is required.");
+        }
+
+        if (!DateTime.TryParse(periodEnd, out var parsedPeriodEnd))
+        {
+            return BadRequest("Invalid periodEnd format.");
+        }
+
         var response = await mediator.Send(new GetTransfersByPeriodEndRequest
         {
             AccountId = accountId,
             PeriodEnd = periodEnd
         });
 
+        if (response.AccountTransfers == null || !response.AccountTransfers.Any())
+        {
+            return NotFound($"No transfers found for account {accountId} and period {periodEnd}.");
+        }
+
         return Ok(response);
     }
+
 }
