d4ac010f8b282ae5ddd084fa449049c330f3a724
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2026-02-24 14:07:12 +0000

    wip

--- src/SFA.DAS.EmployerFinance.Database/Tables/PaymentMetaDataStaging.sql
+++ src/SFA.DAS.EmployerFinance.Database/Tables/PaymentMetaDataStaging.sql
@@ -2,7 +2,8 @@
 (
     [Id] BIGINT IDENTITY(1,1) NOT NULL,
     [PaymentId] UNIQUEIDENTIFIER NOT NULL
-        CONSTRAINT [DF_PaymentMetaDataStaging_PaymentId] DEFAULT ('00000000-0000-0000-0000-000000000000'),
+        CONSTRAINT [DF_PaymentMetaDataStaging_PaymentId] 
+        DEFAULT ('00000000-0000-0000-0000-000000000000'),
     [ProviderName] VARCHAR(MAX) NULL,
     [StandardCode] BIGINT NULL, 
     [FrameworkCode] INT NULL,     
@@ -18,10 +19,7 @@
         CONSTRAINT [DF_PaymentMetaDataStaging_IsHistoricProviderName] DEFAULT (0),
     [CreatedBy] NCHAR(30) NULL, 
     [CorrelationId] UNIQUEIDENTIFIER NULL,
-    CONSTRAINT [PK_PaymentMetaDataStaging] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_PaymentMetaDataStaging_PaymentStaging]
-        FOREIGN KEY ([PaymentId])
-        REFERENCES [employer_financial].[PaymentStaging] ([PaymentId])
+    CONSTRAINT [PK_PaymentMetaDataStaging] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
 GO
 
