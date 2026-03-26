315b0dcc7b2486d0459dd86ea5d4cc85541288e5
SARWAR, Muhammed <Muhammed.SARWAR@EDUCATION.GOV.UK>
2026-03-26 14:25:55 +0000

    ordering using statement

--- src/SFA.DAS.EmployerFinance.Api/Orchestrators/EnglishFractionCalculationDateOrchestrator.cs
+++ src/SFA.DAS.EmployerFinance.Api/Orchestrators/EnglishFractionCalculationDateOrchestrator.cs
@@ -1,6 +1,6 @@
+using System.Threading.Tasks;
 using SFA.DAS.EmployerFinance.Api.Types;
 using SFA.DAS.EmployerFinance.Commands.CreateEnglishFractionCalculationDate;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerFinance.Api.Orchestrators;
 
