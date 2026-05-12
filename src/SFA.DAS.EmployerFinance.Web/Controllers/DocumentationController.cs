70ad24ab9863c8b00def0d45a9fa269a707bed63
killij <51908793+killij@users.noreply.github.com>
2026-05-12 11:05:40 +0100

    FAI-3278 Add specs page (wip)

---
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/DocumentationController.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.EmployerFinance.Web.Controllers;
+
+[Route("[Controller]")]
+public class DocumentationController: Controller
+{
+    [Route("reports/transactions/v2")]
+    public IActionResult TransactionsReportV2()
+    {
+        return View();
+    }
+}
