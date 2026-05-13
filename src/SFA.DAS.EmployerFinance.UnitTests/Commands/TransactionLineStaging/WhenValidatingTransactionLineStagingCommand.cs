8b28a66eeabf879afb16ebdc978003610100b4e3
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2026-05-13 11:07:44 +0100

    Allow negative transaction line staging amounts

---
+++ src/SFA.DAS.EmployerFinance.UnitTests/Commands/TransactionLineStaging/WhenValidatingTransactionLineStagingCommand.cs
@@ -0,0 +1,56 @@
+using SFA.DAS.EmployerFinance.Commands.TransactionLineStaging;
+using SFA.DAS.EmployerFinance.Models.TransactionLineStaging;
+
+namespace SFA.DAS.EmployerFinance.UnitTests.Commands.TransactionLineStaging;
+
+public class WhenValidatingTransactionLineStagingCommand
+{
+    private TransactionLineStagingCommandValidator _validator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _validator = new TransactionLineStagingCommandValidator();
+    }
+
+    [Test]
+    public void Then_Negative_Payment_Amounts_Are_Valid()
+    {
+        var result = _validator.Validate(new TransactionLineStagingCommand
+        {
+            TransactionLines =
+            [
+                new TransactionLineStagingModel
+                {
+                    AccountId = 12345,
+                    DateCreated = new DateTime(2025, 1, 2),
+                    TransactionDate = new DateTime(2025, 1, 1),
+                    TransactionType = 3,
+                    Amount = -100,
+                    SfaCoInvestmentAmount = -20,
+                    EmployerCoInvestmentAmount = -10,
+                    PeriodEnd = "2025-01",
+                    Ukprn = 10000494
+                }
+            ]
+        });
+
+        result.IsValid().Should().BeTrue();
+    }
+
+    [Test]
+    public void Then_Mandatory_Fields_Are_Still_Validated()
+    {
+        var result = _validator.Validate(new TransactionLineStagingCommand
+        {
+            TransactionLines =
+            [
+                new TransactionLineStagingModel()
+            ]
+        });
+
+        result.IsValid().Should().BeFalse();
+        result.ValidationDictionary.Values.Should().Contain("AccountId is mandatory and must be > 0.");
+        result.ValidationDictionary.Values.Should().Contain("TransactionDate is mandatory.");
+    }
+}
