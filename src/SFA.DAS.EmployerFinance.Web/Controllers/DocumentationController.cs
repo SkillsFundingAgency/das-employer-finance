104e43a88108a2df938bfb4a61305972bc517f63
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-05-19 09:33:55 +0100

    FAI-3278 - Add link to new documentation for report

--- src/SFA.DAS.EmployerFinance.Web/Controllers/DocumentationController.cs
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/DocumentationController.cs
@@ -1,9 +1,11 @@
-﻿namespace SFA.DAS.EmployerFinance.Web.Controllers;
+﻿using SFA.DAS.EmployerFinance.Web.Infrastructure;
+
+namespace SFA.DAS.EmployerFinance.Web.Controllers;
 
 [Route("[Controller]")]
 public class DocumentationController: Controller
 {
-    [Route("reports/transactions/v2")]
+    [Route("reports/transactions/v2", Name = RouteNames.DownloadTransactionsReportV2)]
     public IActionResult TransactionsReportV2()
     {
         return View();
