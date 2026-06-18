81d5bd85180945df6d6052b2b3dd78db97410f2e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-06-18 07:59:52 +0100

    FAI-3373 - Change to procs to return transaction type for expiry

--- src/SFA.DAS.EmployerFinance.Database/StoredProcedures/GetExpiredFunds.sql
+++ src/SFA.DAS.EmployerFinance.Database/StoredProcedures/GetExpiredFunds.sql
@@ -4,7 +4,8 @@ AS
 
 SELECT	YEAR(TransactionDate) AS CalendarPeriodYear,
 		MONTH(TransactionDate) AS CalendarPeriodMonth,
-		Amount
+		Amount,
+		TransactionType
 FROM	[employer_financial].[TransactionLine]
 WHERE	AccountId = @AccountId
 		AND TransactionType IN (/*ExpiredFund*/ 5, /*ShortTermExpiredFund*/ 6)
