3deb2093960e79ecb9aa2879844432c678136d56
snipered2003 <snipered@hotmail.com>
2026-08-21 09:12:56 +0100

    Revert "APPMAN-2974"

--- src/SFA.DAS.EmployerFinance/Commands/ExpireAccountFunds/ExpireAccountFundsCommand.cs
+++
@@ -1,9 +0,0 @@
-using SFA.DAS.EmployerFinance.Api.Types;
-
-namespace SFA.DAS.EmployerFinance.Commands.ExpireAccountFunds;
-
-public class ExpireAccountFundsCommand : IRequest<ExpireFundsResponse>
-{
-    public long AccountId { get; set; }
-    public string CorrelationId { get; set; } = string.Empty;
-}
