9a5f61f43d588b6a69419fd3bdc35c4b8aeb3371
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-09-10 08:51:01 +0100

    FAI-3433 - Dacpac issue

--- src/SFA.DAS.EmployerFinance.Database/StoredProcedures/GetTransactionLines_ByAccountId.sql
+++ src/SFA.DAS.EmployerFinance.Database/StoredProcedures/GetTransactionLines_ByAccountId.sql
@@ -5,53 +5,53 @@
 AS
 
     SELECT
-		  tl.[AccountId],
-		  tl.TransactionType,
-		  MAX(tl.TransactionDate) as TransactionDate,
-		  Sum(tl.Amount) as Amount,
-		  tl.Ukprn,
-		  tl.DateCreated,
-		  tl.SfaCoInvestmentAmount,
-		  tl.EmployerCoInvestmentAmount,
-		  tl.PeriodEnd,
-		  ld.PayrollYear,
-		  ld.PayrollMonth,
-		  tl.TransferSenderAccountId as SenderAccountId,
-		  tl.TransferSenderAccountName as SenderAccountName,
-		  tl.TransferReceiverAccountId as ReceiverAccountId,
-		  tl.TransferReceiverAccountName as ReceiverAccountName,
-		  MAX(pmd.ProviderName) as ProviderName
-  FROM	[employer_financial].[TransactionLine] tl
-		LEFT JOIN [employer_financial].LevyDeclaration ld
-			on ld.submissionid = tl.submissionid
-		LEFT JOIN [employer_financial].[AccountTransfers] transfers
-			ON transfers.SenderAccountId = tl.TransferSenderAccountId
-			AND transfers.ReceiverAccountId = tl.TransferReceiverAccountId
-			AND transfers.PeriodEnd = tl.PeriodEnd
-		LEFT JOIN [employer_financial].[Payment] p
-			ON p.AccountId = transfers.ReceiverAccountId
-			AND p.ApprenticeshipId = transfers.ApprenticeshipId
-			AND p.PeriodEnd = transfers.PeriodEnd
-		LEFT JOIN [employer_financial].[PaymentMetaData] pmd
-			ON pmd.Id = p.PaymentMetaDataId
-  WHERE tl.AccountId = @accountId
-		AND tl.DateCreated >= @fromDate
-		AND DateCreated <= @toDate
-  GROUP BY
-		tl.DateCreated,
-		tl.AccountId,
-		tl.UKPRN,
-		tl.SfaCoInvestmentAmount,
-		tl.EmployerCoInvestmentAmount,
-		tl.TransactionType,
-		tl.PeriodEnd,
-		ld.PayrollMonth,
-		ld.PayrollYear,
-		tl.TransferSenderAccountId,
-		tl.TransferSenderAccountName,
-		tl.TransferReceiverAccountId,
-		tl.TransferReceiverAccountName
-order by 
-		DateCreated desc, 
-		TransactionType desc, 
-		ukprn desc
+        tl.[AccountId],
+        tl.TransactionType,
+        MAX(tl.TransactionDate) as TransactionDate,
+        Sum(tl.Amount) as Amount,
+        tl.Ukprn,
+        tl.DateCreated,
+        tl.SfaCoInvestmentAmount,
+        tl.EmployerCoInvestmentAmount,
+        tl.PeriodEnd,
+        ld.PayrollYear,
+        ld.PayrollMonth,
+        tl.TransferSenderAccountId as SenderAccountId,
+        tl.TransferSenderAccountName as SenderAccountName,
+        tl.TransferReceiverAccountId as ReceiverAccountId,
+        tl.TransferReceiverAccountName as ReceiverAccountName,
+        MAX(pmd.ProviderName) as ProviderName
+    FROM	[employer_financial].[TransactionLine] tl
+                LEFT JOIN [employer_financial].LevyDeclaration ld
+                          on ld.submissionid = tl.submissionid
+                LEFT JOIN [employer_financial].[AccountTransfers] transfers
+                          ON transfers.SenderAccountId = tl.TransferSenderAccountId
+                              AND transfers.ReceiverAccountId = tl.TransferReceiverAccountId
+                              AND transfers.PeriodEnd = tl.PeriodEnd
+                LEFT JOIN [employer_financial].[Payment] p
+                          ON p.AccountId = transfers.ReceiverAccountId
+                              AND p.ApprenticeshipId = transfers.ApprenticeshipId
+                              AND p.PeriodEnd = transfers.PeriodEnd
+                LEFT JOIN [employer_financial].[PaymentMetaData] pmd
+                          ON pmd.Id = p.PaymentMetaDataId
+    WHERE tl.AccountId = @accountId
+      AND tl.DateCreated >= @fromDate
+      AND tl.DateCreated <= @toDate
+    GROUP BY
+        tl.DateCreated,
+        tl.AccountId,
+        tl.UKPRN,
+        tl.SfaCoInvestmentAmount,
+        tl.EmployerCoInvestmentAmount,
+        tl.TransactionType,
+        tl.PeriodEnd,
+        ld.PayrollMonth,
+        ld.PayrollYear,
+        tl.TransferSenderAccountId,
+        tl.TransferSenderAccountName,
+        tl.TransferReceiverAccountId,
+        tl.TransferReceiverAccountName
+    order by
+        tl.DateCreated desc,
+        tl.TransactionType desc,
+        tl.ukprn desc
